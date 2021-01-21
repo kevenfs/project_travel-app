109bac71b359e8cb597318c9811b181e20f6578c
Keven Fortin-Simard <keven@hotelwizards.com>
2021-01-20 22:01:08 -0500

    Add bootstrap

--- webpack.dev.js
+++ webpack.dev.js
@@ -19,9 +19,25 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader"
         }, {
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
+            test: /\.(scss)$/,
+            use: [{
+                loader: 'style-loader', // inject CSS to page
+            }, {
+                loader: 'css-loader', // translates CSS into CommonJS modules
+            }, {
+                loader: 'postcss-loader', // Run post css actions
+                options: {
+                    plugins: function () { // post css plugins, can be exported to postcss.config.js
+                        return [
+                            require('precss'),
+                            require('autoprefixer')
+                        ];
+                    }
+                }
+            }, {
+                loader: 'sass-loader' // compiles Sass to CSS
+            }]
+        }, ]
     },
     plugins: [
         new HtmlWebPackPlugin({
--- webpack.prod.js
+++ webpack.prod.js
@@ -22,10 +22,31 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader"
         }, {
-            test: /\.scss$/,
-            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-        }]
+            test: /\.(scss)$/,
+            use: [{
+                    loader: MiniCssExtractPlugin,
+                },
+                {
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run post css actions
+                    options: {
+                        plugins: function () { // post css plugins, can be exported to postcss.config.js
+                            return [
+                                require('precss'),
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }
+            ]
+        }, ]
     },
+
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
