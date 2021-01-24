20f739768977114455cd1d126b31e56b17c4078b
Keven Fortin-Simard <keven@hotelwizards.com>
2021-01-23 19:24:51 -0500

    Delete Bootstrap

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,29 +15,15 @@ module.exports = {
     devtool: 'source-map',
     module: {
         rules: [{
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }, {
-            test: /\.(scss)$/,
-            use: [{
-                loader: 'style-loader', // inject CSS to page
-            }, {
-                loader: 'css-loader', // translates CSS into CommonJS modules
-            }, {
-                loader: 'postcss-loader', // Run post css actions
-                options: {
-                    plugins: function () { // post css plugins, can be exported to postcss.config.js
-                        return [
-                            require('precss'),
-                            require('autoprefixer')
-                        ];
-                    }
-                }
-            }, {
-                loader: 'sass-loader' // compiles Sass to CSS
-            }]
-        }, ]
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
     },
     plugins: [
         new HtmlWebPackPlugin({
